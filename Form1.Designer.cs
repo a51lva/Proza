201c9ea1f62957a2b8d3052f76ad0f9adb92ab98
Acilio Silva <eng.acilio@gmail.com>
2019-04-26 11:26:41 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Proza
+{
+    partial class Proza
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Proza));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.history = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.actionsTitle = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.runButton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.scheduleTwenty = new System.Windows.Forms.RadioButton();
+            this.scheduleThirty = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.history);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.actionsTitle);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            // 
+            // history
+            // 
+            this.history.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.history.ForeColor = System.Drawing.Color.IndianRed;
+            resources.ApplyResources(this.history, "history");
+            this.history.Name = "history";
+            this.history.ReadOnly = true;
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // actionsTitle
+            // 
+            resources.ApplyResources(this.actionsTitle, "actionsTitle");
+            this.actionsTitle.Name = "actionsTitle";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.saveButton);
+            this.panel2.Controls.Add(this.runButton);
+            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.scheduleTwenty);
+            this.panel2.Controls.Add(this.scheduleThirty);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Controls.Add(this.label1);
+            resources.ApplyResources(this.panel2, "panel2");
+            this.panel2.Name = "panel2";
+            // 
+            // saveButton
+            // 
+            resources.ApplyResources(this.saveButton, "saveButton");
+            this.saveButton.Name = "saveButton";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // runButton
+            // 
+            this.runButton.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            resources.ApplyResources(this.runButton, "runButton");
+            this.runButton.Name = "runButton";
+            this.runButton.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            resources.ApplyResources(this.radioButton1, "radioButton1");
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // scheduleTwenty
+            // 
+            resources.ApplyResources(this.scheduleTwenty, "scheduleTwenty");
+            this.scheduleTwenty.Name = "scheduleTwenty";
+            this.scheduleTwenty.TabStop = true;
+            this.scheduleTwenty.UseVisualStyleBackColor = true;
+            // 
+            // scheduleThirty
+            // 
+            resources.ApplyResources(this.scheduleThirty, "scheduleThirty");
+            this.scheduleThirty.Name = "scheduleThirty";
+            this.scheduleThirty.TabStop = true;
+            this.scheduleThirty.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // checkBox2
+            // 
+            resources.ApplyResources(this.checkBox2, "checkBox2");
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Proza
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Proza";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button runButton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton scheduleTwenty;
+        private System.Windows.Forms.RadioButton scheduleThirty;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label actionsTitle;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox history;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proza
+{
+    public partial class Proza : Form
+    {
+        public Proza()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean deleteEval()
+        {
+            const String evalDirectory = "%USERPROFILE%\\.IntelliJIdea2018.2\\config\\eval";
+
+            if (Directory.Exists(evalDirectory))
+            {
+                try
+                {
+                    Directory.Delete(@evalDirectory, true);
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    history.Text = e.Message;
+                    Console.WriteLine("The process failed: {0}", e.Message);
+                    if (!string.IsNullOrWhiteSpace(history.Text))
+                    {
+                        history.AppendText("\r\n" + e.Message);
+                    }
+                    else
+                    {
+                        history.AppendText(e.Message);
+                    }
+                    history.ScrollToCaret();
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
